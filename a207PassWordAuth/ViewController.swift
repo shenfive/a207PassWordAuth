cc73d665aad3367c7cb2b3027dab04756e020fbd
Danny Shen <shenfive@gmail.com>
2020-08-29 17:10:03 +0800

    收鍵盤

--- a207PassWordAuth/ViewController.swift
+++ a207PassWordAuth/ViewController.swift
@@ -25,6 +25,10 @@ class ViewController: UIViewController {
         }
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     func signInUI(){
         print("己登入")
         if let user = Auth.auth().currentUser?.email{
